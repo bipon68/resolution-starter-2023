fa0523792eccc6b003ba02ace14c1f08ea48b4ad
bipon68 <bipon68@users.noreply.github.com>
2023-01-19 13:31:29 +0600

    #3 REST API GET Method setup

--- postgres-book-api/index.js
+++ postgres-book-api/index.js
@@ -5,3 +5,25 @@ const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`server is running at http://localhost:${PORT}`)
 })
+
+// GET /books -> return all the books
+app.get("/books", async(req, res) => {
+    try {
+        res.status(200).json({message: "Books are returned"})
+    } catch (error) {
+        res.json({error: error.message})
+    }
+})
+
+// GET /books/:id -> return return a specific book
+app.get("/books/:id", async(req, res) => {
+    try {
+        const {id} = req.params;
+        res.status(200).json({message: `Specific book is returned with id: ${id}` })
+    } catch (error) {
+        res.json({error: error.message})
+    }
+})
+// POST /books -> create book
+// DELETE /books/:id -> delete a book
+// PUT /books/:id -> update a book
